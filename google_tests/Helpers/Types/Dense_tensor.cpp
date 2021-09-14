dc018fc996c3176a208a1a91cfc1a1d2f42582fa
faccus <federicoluca.facchinetti@mail.polimi.it>
2021-09-14 11:25:55 +0200

    Fixed a bug of test

--- google_tests/Helpers/Types/Dense_tensor.cpp
+++ google_tests/Helpers/Types/Dense_tensor.cpp
@@ -7,7 +7,9 @@
 
 TEST(DenseTensorTest, ConstuctorAndMemoryUsage) {
   Dense_tensor d(onnx::TensorProto_DataType_INT64,
-                 {1, 1, 2, 2}); // total memory 2*2*64=256
+                 {1, 1, 2, 2}); // total memory 2*2*64=256 bits
 
-  ASSERT_TRUE(d.compute_memory_usage() == 256);
+  auto res = d.compute_memory_usage();
+
+  ASSERT_TRUE(res == 4 * sizeof(int64_t));
 }
