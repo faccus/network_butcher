53076bffa2e0323a02f796cd460a0f5f578c4e0b
faccus <federicoluca.facchinetti@mail.polimi.it>
2022-02-17 16:59:42 +0100

    Changed Node and added Content

---
+++ src/Farmer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by faccus on 17/02/22.
+//
+
+#include "Farmer.h"
---
+++ src/Farmer.h
@@ -0,0 +1,13 @@
+ù //
+// Created by faccus on 17/02/22.
+//
+
+#ifndef NETWORK_BUTCHER_FARMER_H
+#define NETWORK_BUTCHER_FARMER_H
+
+
+  class Farmer
+{};
+
+
+#endif // NETWORK_BUTCHER_FARMER_H
