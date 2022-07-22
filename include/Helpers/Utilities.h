e521f733ad8b06a8b174c19406c636d66d43abe7
faccus <federicoluca.facchinetti@gmail.com>
2022-07-22 10:14:21 -0700

    Added some trim functions

--- include/Helpers/Utilities.h
+++ include/Helpers/Utilities.h
@@ -58,6 +58,55 @@ namespace utilities
       }
   }
 
+  // trim from start (in place)
+  static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+              return !std::isspace(ch);
+            }));
+  }
+
+  // trim from end (in place)
+  static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+              return !std::isspace(ch);
+            }).base(), s.end());
+  }
+
+  // trim from both ends (in place)
+  static inline void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+  }
+
+  // Makes the string lower case (in place)
+  static inline void to_lowercase(std::string &s) {
+    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+  }
+
+  // trim from start (copying)
+  static inline std::string ltrim_copy(std::string s) {
+    ltrim(s);
+    return s;
+  }
+
+  // trim from end (copying)
+  static inline std::string rtrim_copy(std::string s) {
+    rtrim(s);
+    return s;
+  }
+
+  // trim from both ends (copying)
+  static inline std::string trim_copy(std::string s) {
+    trim(s);
+    return s;
+  }
+
+  // Makes the string lower case (copying)
+  static inline std::string to_lowercase_copy(std::string s) {
+    to_lowercase(s);
+    return s;
+  }
+
 } // namespace utilities
 
 #endif // NETWORK_BUTCHER_UTILITIES_H
