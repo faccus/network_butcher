4725670a91c282ab10739622bc5a2c7e43c587f7
faccus <federicoluca.facchinetti@gmail.com>
2022-08-05 16:42:51 +0200

    Fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ main(int argc, char **argv)
   GetPot            command_line(argc, argv);
   std::string const config_path = command_line("config_file", "config.conf");
 
-  General_Manager::boot(config_path, true);
+  network_butcher_io::General_Manager::boot(config_path, true);
 
   return 0;
 }
