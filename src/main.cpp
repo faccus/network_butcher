235b3b45cd0549ed359e9beea3e6865f57c19802
faccus <federicoluca.facchinetti@gmail.com>
2022-08-05 17:55:07 +0200

    Basic test main

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,66 @@ main(int argc, char **argv)
 }
 */
 
-#include "yaml-cpp/yaml.h"
+#include "../include/IO_Manager.h"
 
 #include <iostream>
+#include <yaml-cpp/yaml.h>
+
+struct network_domain
+{
+  std::string name;
+
+  std::size_t bandwidth;
+  double      access_delay;
+};
 
 int
 main()
 {
+  std::map<std::string, network_domain>        network_domains;
+  std::map<std::string, std::set<std::string>> subdomains;
+
+  std::map<std::size_t, std::string> layer_to_domain;
+  std::map<std::string, std::size_t> name_to_layer;
+
+  {
+    YAML::Node network_domains_yaml = YAML::LoadFile("candidate_resources.yaml")["System"]["NetworkDomains"];
+    for (auto const &domain : network_domains_yaml)
+      {
+        auto const name = domain.first.as<std::string>();
+
+        network_domains[name] = {domain.second["name"].as<std::string>(),
+                                 domain.second["Bandwidth"].as<std::size_t>(),
+                                 domain.second["AccessDelay"].as<double>()};
+
+        if (domain.second["subNetworkDomains"].size() > 0)
+          {
+            for (auto const &subdomain : domain.second["subNetworkDomains"])
+              subdomains[name].insert(subdomain.as<std::string>());
+          }
+
+        if (domain.second["ComputationalLayers"])
+          {
+            YAML::Node layers = domain.second["ComputationalLayers"];
+            for (YAML::const_iterator layer_it = layers.begin(); layer_it != layers.end(); ++layer_it)
+              {
+                auto const id       = layer_it->second["number"].as<std::size_t>();
+                layer_to_domain[id] = name;
+
+                if (layer_it->second["Resources"])
+                  {
+                    YAML::Node resources = layer_it->second["Resources"];
+                    for (YAML::const_iterator resource_it = resources.begin(); resource_it != resources.end();
+                         ++resource_it)
+                      {
+                        name_to_layer[resource_it->second["name"].as<std::string>()] = id;
+                      }
+                  }
+              }
+          }
+      }
+  }
+
   YAML::Node annotations = YAML::LoadFile("annotations.yaml"); // candidate_deployments candidate_resources
   YAML::Node components  = YAML::LoadFile("candidate_deployments.yaml")["Components"];
 
@@ -36,28 +89,45 @@ main()
 
   for (auto const &model_friendly_name : to_deploy)
     {
+      std::map<std::string, std::size_t> devices_ram;
+      std::set<std::size_t>              layers;
+
       for (YAML::const_iterator it = components.begin(); it != components.end(); ++it)
         {
           if (it->second["name"] &&
               it->second["name"].as<std::string>().find(model_friendly_name) != std::string::npos &&
               it->second["name"].as<std::string>().find("partitionX") != std::string::npos)
-            {}
-        }
-    }
-  for (YAML::const_iterator it = components.begin(); it != components.end(); ++it)
-    {
-      if (it->second["name"] && to_deploy.contains(it->second["name"].as<std::string>()))
-        {
-          YAML::Node execution_layers = it->second["candidateExecutionLayers"];
+            {
+              YAML::Node execution_layers = it->second["candidateExecutionLayers"];
 
-          std::set<std::size_t> layers;
-          for (YAML::const_iterator it2 = execution_layers.begin(); it2 != execution_layers.end(); ++it2)
-            layers.emplace(it2->second.as<std::size_t>());
 
-          YAML::Node devices = it->second["Containers"];
-          for (YAML::const_iterator device_it = devices.begin(); device_it != devices.end(); ++device_it)
-            {}
+              for (auto const &idw : execution_layers)
+                {
+                  layers.emplace(idw.as<std::size_t>());
+                }
+
+              YAML::Node devices = it->second["Containers"];
+
+              for (YAML::const_iterator device_it = devices.begin(); device_it != devices.end(); ++device_it)
+                {
+                  devices_ram[device_it->second["candidateExecutionResources"][0].as<std::string>()] =
+                    device_it->second["memorySize"].as<std::size_t>();
+                }
+            }
         }
+
+      Parameters params;
+      params.devices = std::vector<Device>(devices_ram.size());
+
+      {
+        std::size_t index = 0;
+        for (auto const &device : devices_ram)
+          {
+            auto &ref          = params.devices[index];
+            ref.maximum_memory = device.second;
+            ref.id             = index++;
+          }
+      }
     }
 
 
