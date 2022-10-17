90939b490fec6c5f72a6dbe177b2c46780f416b4
faccus <federicoluca.facchinetti@gmail.com>
2022-10-17 12:27:36 +0200

    Fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
-
 #include "../include/APSC/GetPot"
 #include "../include/General_Manager.h"
+
+/*
+int
+main(int argc, char **argv)
+{
+  std::string const annotations_path           = "annotations.yaml";
+  std::string const candidate_deployments_path = "candidate_deployments.yaml";
+  std::string const candidate_resources_path   = "candidate_resources.yaml";
+
+  auto params = network_butcher_io::IO_Manager::read_parameters_yaml(candidate_resources_path,
+                                                                     candidate_deployments_path,
+                                                                     annotations_path);
+
+  for (std::size_t i = 0; i < params.size(); ++i)
+    {
+      auto &param = params[i];
+
+      param.export_directory = "ksp_result_yaml_" + std::to_string(i);
+      network_butcher_io::General_Manager::boot(param, true);
+    }
+
+  return 0;
+}
+*/
+
 
 int
 main(int argc, char **argv)
@@ -9,44 +33,50 @@ main(int argc, char **argv)
 
   std::cout << "Network Butcher v1.0" << std::endl;
 
-  if (command_line.size() == 2)
+  if (command_line.search(2, "--help", "-h"))
     {
-      if (command_line.search("--help"))
-        {
-          std::cout << std::endl << "Command usage: " << std::endl;
-          std::cout << "#1: ./network_butcher config_file=config.conf" << std::endl;
-          std::cout << "#2: ./network_butcher annotations=annotations.yaml "
-                       "candidate_deployments=candidate_deployments.yaml candidate_resources=candidate_resources.yaml"
-                    << std::endl;
-        }
-      else
+      std::cout << std::endl << "Command usage: " << std::endl;
+      std::cout << "#1: ./network_butcher config_file=config.conf" << std::endl;
+      std::cout << "#2: ./network_butcher annotations=annotations.yaml "
+                   "candidate_deployments=candidate_deployments.yaml candidate_resources=candidate_resources.yaml"
+                << std::endl;
+    }
+  else
+    {
+      if (command_line.vector_variable_size("config_file"))
         {
           std::string const config_path = command_line("config_file", "config.conf");
 
           network_butcher_io::General_Manager::boot(config_path, true);
         }
-    }
-#if YAML_CPP_ACTIVE
-  else if (command_line.size() == 4)
-    {
-      std::string const annotations_path = command_line("annotations", "annotations.yaml");
-      std::string const candidate_deployments_path =
-        command_line("candidate_deployments", "candidate_deployments.yaml");
-      std::string const candidate_resources_path = command_line("candidate_resources", "candidate_resources.yaml");
-
-      auto params = network_butcher_io::IO_Manager::read_parameters_yaml(candidate_resources_path,
-                                                                         candidate_deployments_path,
-                                                                         annotations_path);
-
-      for (std::size_t i = 0; i < params.size(); ++i)
+      else if (command_line.vector_variable_size("annotations") ||
+               command_line.vector_variable_size("candidate_deployments") ||
+               command_line.vector_variable_size("candidate_resources"))
         {
-          auto &param = params[i];
+#if YAML_CPP_ACTIVE
+          std::string const annotations_path = command_line("annotations", "annotations.yaml");
+          std::string const candidate_deployments_path =
+            command_line("candidate_deployments", "candidate_deployments.yaml");
+          std::string const candidate_resources_path = command_line("candidate_resources", "candidate_resources.yaml");
 
-          param.export_directory = std::to_string(i);
-          network_butcher_io::General_Manager::boot(param, true);
+          auto params = network_butcher_io::IO_Manager::read_parameters_yaml(candidate_resources_path,
+                                                                             candidate_deployments_path,
+                                                                             annotations_path);
+
+          for (std::size_t i = 0; i < params.size(); ++i)
+            {
+              auto &param = params[i];
+
+              param.export_directory = "ksp_result_yaml_" + std::to_string(i);
+              network_butcher_io::General_Manager::boot(param, true);
+            }
+#else
+          std::cout << "The library Yaml-Cpp is required to read .yaml files. Please, check the CMakeList.txt "
+                       "configuration file."
+                    << std::endl;
+#endif
         }
     }
-#endif
 
   return 0;
 }
