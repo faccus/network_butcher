129781a439dacaaedfd425be0b8969f5ffb6f3a3
faccus <federicoluca.facchinetti@gmail.com>
2022-10-20 16:38:15 +0200

    Changed static class with namespace

--- include/IO_Interaction/Onnx_model_reconstructor_helpers.h
+++ include/IO_Interaction/Onnx_model_reconstructor_helpers.h
@@ -10,102 +10,97 @@
 
 #include <sstream>
 
-namespace network_butcher_io
+namespace network_butcher_io::Onnx_model_reconstructor_helpers
 {
-  class Onnx_model_reconstructor_helpers
+  enum IO_Type
   {
-  public:
-    enum IO_Type
-    {
-      Input,
-      ValueInfo,
-      Initializer,
-      Initializer_Input,
-      Initializer_ValueInfo,
-      NoConnection
-    };
-
-    /// From the model and the name of the Tensor, it will return if its found and the position
-    /// \param original_model The model
-    /// \param communication_node_name THe name of the tensor
-    /// \return Found and the position
-    static std::pair<bool, google::protobuf::RepeatedPtrField<onnx::ValueInfoProto>::const_iterator>
-    get_type(const onnx::ModelProto &original_model, const std::string &communication_node_name);
-
-    /// From the original model, it will add basic information to the new_model
-    /// \param original_model THe original model
-    /// \param new_model The model to be prepared
-    static void
-    prepare_new_model(const onnx::ModelProto &original_model, onnx::ModelProto &new_model);
-
-    /// From the original model, it will return a new graph with the same name and documentation string of the graph of
-    /// the original model
-    /// \param original_model The original model
-    /// \return The "new" graph
-    static onnx::GraphProto *
-    prepare_new_graph(const onnx::ModelProto &original_model);
-
-    static std::pair<bool, google::protobuf::RepeatedPtrField<onnx::TensorProto>::const_iterator>
-    get_initializer(const onnx::ModelProto &original_model, const std::string &name);
-
-    /// It will add to the graph the inputs of the node to either to input, to value_info or to initializer
-    /// \param model_graph The model
-    /// \param graph The graph
-    /// \param node The node
-    static void
-    add_node_ios_nodes(
-      onnx::GraphProto      *graph,
-      const onnx::NodeProto *node,
-      std::unordered_map<
-        std::string,
-        std::pair<network_butcher_io::Onnx_model_reconstructor_helpers::IO_Type,
-                  std::pair<google::protobuf::RepeatedPtrField<onnx::ValueInfoProto>::const_iterator,
-                            google::protobuf::RepeatedPtrField<onnx::TensorProto>::const_iterator>>> const
-        &preprocessed_ios_nodes);
-
-    /// It will find the inputs/outputs and/or initializers of a given graph
-    /// \param model_graph The graph of the given model
-    /// \return A map that associates to the name of the tensor a pair describing if it's a in/out/init and its position
-    /// in the object graph
-    static std::unordered_map<
-      std::string,
-      std::pair<IO_Type,
-                std::pair<google::protobuf::RepeatedPtrField<onnx::ValueInfoProto>::const_iterator,
-                          google::protobuf::RepeatedPtrField<onnx::TensorProto>::const_iterator>>>
-    process_node_ios_nodes(const onnx::GraphProto &model_graph);
-
-    /// It will add a specific collections of nodes of the model_graph to the current_edited_graph
-    /// \param link_id_nodeproto The map that associates the id of a node in the internal graph representation to its
-    /// id in the original graph
-    /// \param model_graph The original graph
-    /// \param nodes The (internal) ids of the nodes to be added
-    /// \param current_edited_graph The current graph
-    /// \param preprocessed_ios_nodes The output of process_node_ios_nodes
-    static void
-    add_nodes(const std::map<node_id_type, node_id_type> &link_id_nodeproto,
-              const onnx::GraphProto                     &model_graph,
-              const std::set<node_id_type>               &nodes,
-              onnx::GraphProto                           *current_edited_graph,
-              std::unordered_map<
-                std::string,
-                std::pair<network_butcher_io::Onnx_model_reconstructor_helpers::IO_Type,
-                          std::pair<google::protobuf::RepeatedPtrField<onnx::ValueInfoProto>::const_iterator,
-                                    google::protobuf::RepeatedPtrField<onnx::TensorProto>::const_iterator>>> const
-                &preprocessed_ios_nodes);
-
-
-    /// Adds the "missing" inputs of the current graph. They represent the new inputs for the new model
-    /// \param original_model The original model
-    /// \param current_edited_graph The new graph
-    static void
-    add_missing_inputs(const onnx::ModelProto &original_model, onnx::GraphProto *current_edited_graph);
-
-    /// Adds the "missing" outputs of the current graph. They represent the new outputs for the new model
-    /// \param original_model The original model
-    /// \param current_edited_graph The new graph
-    static void
-    add_missing_outputs(const onnx::ModelProto &original_model, onnx::GraphProto *current_edited_graph);
+    Input,
+    ValueInfo,
+    Initializer,
+    Initializer_Input,
+    Initializer_ValueInfo,
+    NoConnection
   };
-} // namespace network_butcher_io
+
+  /// From the model and the name of the Tensor, it will return if its found and the position
+  /// \param original_model The model
+  /// \param communication_node_name THe name of the tensor
+  /// \return Found and the position
+  std::pair<bool, google::protobuf::RepeatedPtrField<onnx::ValueInfoProto>::const_iterator>
+  get_type(const onnx::ModelProto &original_model, const std::string &communication_node_name);
+
+  /// From the original model, it will add basic information to the new_model
+  /// \param original_model THe original model
+  /// \param new_model The model to be prepared
+  void
+  prepare_new_model(const onnx::ModelProto &original_model, onnx::ModelProto &new_model);
+
+  /// From the original model, it will return a new graph with the same name and documentation string of the graph of
+  /// the original model
+  /// \param original_model The original model
+  /// \return The "new" graph
+  onnx::GraphProto *
+  prepare_new_graph(const onnx::ModelProto &original_model);
+
+  std::pair<bool, google::protobuf::RepeatedPtrField<onnx::TensorProto>::const_iterator>
+  get_initializer(const onnx::ModelProto &original_model, const std::string &name);
+
+  /// It will add to the graph the inputs of the node to either to input, to value_info or to initializer
+  /// \param model_graph The model
+  /// \param graph The graph
+  /// \param node The node
+  void
+  add_node_ios_nodes(
+    onnx::GraphProto      *graph,
+    const onnx::NodeProto *node,
+    std::unordered_map<
+      std::string,
+      std::pair<network_butcher_io::Onnx_model_reconstructor_helpers::IO_Type,
+                std::pair<google::protobuf::RepeatedPtrField<onnx::ValueInfoProto>::const_iterator,
+                          google::protobuf::RepeatedPtrField<onnx::TensorProto>::const_iterator>>> const
+      &preprocessed_ios_nodes);
+
+  /// It will find the inputs/outputs and/or initializers of a given graph
+  /// \param model_graph The graph of the given model
+  /// \return A map that associates to the name of the tensor a pair describing if it's a in/out/init and its position
+  /// in the object graph
+  std::unordered_map<std::string,
+                     std::pair<IO_Type,
+                               std::pair<google::protobuf::RepeatedPtrField<onnx::ValueInfoProto>::const_iterator,
+                                         google::protobuf::RepeatedPtrField<onnx::TensorProto>::const_iterator>>>
+  process_node_ios_nodes(const onnx::GraphProto &model_graph);
+
+  /// It will add a specific collections of nodes of the model_graph to the current_edited_graph
+  /// \param link_id_nodeproto The map that associates the id of a node in the internal graph representation to its
+  /// id in the original graph
+  /// \param model_graph The original graph
+  /// \param nodes The (internal) ids of the nodes to be added
+  /// \param current_edited_graph The current graph
+  /// \param preprocessed_ios_nodes The output of process_node_ios_nodes
+  void
+  add_nodes(const std::map<node_id_type, node_id_type> &link_id_nodeproto,
+            const onnx::GraphProto                     &model_graph,
+            const std::set<node_id_type>               &nodes,
+            onnx::GraphProto                           *current_edited_graph,
+            std::unordered_map<
+              std::string,
+              std::pair<network_butcher_io::Onnx_model_reconstructor_helpers::IO_Type,
+                        std::pair<google::protobuf::RepeatedPtrField<onnx::ValueInfoProto>::const_iterator,
+                                  google::protobuf::RepeatedPtrField<onnx::TensorProto>::const_iterator>>> const
+              &preprocessed_ios_nodes);
+
+
+  /// Adds the "missing" inputs of the current graph. They represent the new inputs for the new model
+  /// \param original_model The original model
+  /// \param current_edited_graph The new graph
+  void
+  add_missing_inputs(const onnx::ModelProto &original_model, onnx::GraphProto *current_edited_graph);
+
+  /// Adds the "missing" outputs of the current graph. They represent the new outputs for the new model
+  /// \param original_model The original model
+  /// \param current_edited_graph The new graph
+  void
+  add_missing_outputs(const onnx::ModelProto &original_model, onnx::GraphProto *current_edited_graph);
+} // namespace network_butcher_io::Onnx_model_reconstructor_helpers
 
 #endif // NETWORK_BUTCHER_ONNX_MODEL_RECONSTRUCTOR_HELPERS_H
--- include/IO_Interaction/Yaml_importer_helpers.h
+++ include/IO_Interaction/Yaml_importer_helpers.h
@@ -12,53 +12,48 @@
 #include <yaml-cpp/yaml.h>
 
 
-namespace network_butcher_io
+namespace network_butcher_io::Yaml_importer_helpers
 {
-  class Yaml_importer_helpers
-  {
-  public:
-    /// Reads the candidate deployments file and returns {network_domains, subdomain_to_domain, devices_map}
-    /// \param candidate_resources_path The candidate resources file path
-    /// \return Returns {network_domains, subdomain_to_domain, devices_map}
-    static std::
-      tuple<std::map<std::string, network_domain>, std::map<std::string, std::string>, std::map<std::string, device>>
-      read_candidate_resources(const std::string &candidate_resources_path);
+  /// Reads the candidate deployments file and returns {network_domains, subdomain_to_domain, devices_map}
+  /// \param candidate_resources_path The candidate resources file path
+  /// \return Returns {network_domains, subdomain_to_domain, devices_map}
+  std::tuple<std::map<std::string, network_domain>, std::map<std::string, std::string>, std::map<std::string, device>>
+  read_candidate_resources(const std::string &candidate_resources_path);
 
-    /// Search for the bandwidth and network access time from the first domain to the second one
-    /// \param network_domains The collection of network_domains
-    /// \param subdomain_to_domain Map from the subdomains to the associated domains
-    /// \param first_domain The first domain
-    /// \param second_domain The second domain
-    /// \return Bandwidth and network access time
-    static std::pair<bandwidth_type, bandwidth_type>
-    find_bandwidth(std::map<std::string, network_domain> const &network_domains,
-                   std::map<std::string, std::string> const    &subdomain_to_domain,
-                   std::string                                  first_domain,
-                   std::string                                  second_domain);
+  /// Search for the bandwidth and network access time from the first domain to the second one
+  /// \param network_domains The collection of network_domains
+  /// \param subdomain_to_domain Map from the subdomains to the associated domains
+  /// \param first_domain The first domain
+  /// \param second_domain The second domain
+  /// \return Bandwidth and network access time
+  std::pair<bandwidth_type, bandwidth_type>
+  find_bandwidth(std::map<std::string, network_domain> const &network_domains,
+                 std::map<std::string, std::string> const    &subdomain_to_domain,
+                 std::string                                  first_domain,
+                 std::string                                  second_domain);
 
 
-    /// Reads the annotation file and extract the general information of the model to be partitioned
-    /// \param annotations_path The annotation path
-    /// \return Map that associates to the model name the avaible ram and vram
-    static std::map<std::string, std::pair<std::size_t, std::size_t>>
-    read_annotations(const std::string &annotations_path);
+  /// Reads the annotation file and extract the general information of the model to be partitioned
+  /// \param annotations_path The annotation path
+  /// \return Map that associates to the model name the avaible ram and vram
+  std::map<std::string, std::pair<std::size_t, std::size_t>>
+  read_annotations(const std::string &annotations_path);
 
 
-    /// Reads the candidate deployments file and returns the list of containers that can be deployed for every partition
-    /// of every model
-    /// \param candidate_deployments_path The candidate deployments file path
-    /// \param models The collection of models (with ram and vram)
-    /// \param devices_map The collection of devices
-    /// \return A map that associates to every model the collection of devices for each possible partition
-    static std::map<std::string, std::vector<std::map<std::string, std::size_t>>>
-    read_candidate_deployments(std::string const                   &candidate_deployments_path,
-                               std::map<std::string, std::pair<std::size_t, std::size_t>> const &models,
-                               std::map<std::string, device> const &devices_map);
+  /// Reads the candidate deployments file and returns the list of containers that can be deployed for every partition
+  /// of every model
+  /// \param candidate_deployments_path The candidate deployments file path
+  /// \param models The collection of models (with ram and vram)
+  /// \param devices_map The collection of devices
+  /// \return A map that associates to every model the collection of devices for each possible partition
+  std::map<std::string, std::vector<std::map<std::string, std::size_t>>>
+  read_candidate_deployments(std::string const &candidate_deployments_path,
+                             std::map<std::string, std::pair<std::size_t, std::size_t>> const &models,
+                             std::map<std::string, device> const                              &devices_map);
 
 
-    static std::vector<std::vector<std::pair<std::string, std::size_t>>>
-    get_devices_for_partitions(const std::vector<std::map<std::string, std::size_t>> &devices_ram);
-  };
-} // namespace network_butcher_io
+  std::vector<std::vector<std::pair<std::string, std::size_t>>>
+  get_devices_for_partitions(const std::vector<std::map<std::string, std::size_t>> &devices_ram);
+} // namespace network_butcher_io::Yaml_importer_helpers
 
 #endif // NETWORK_BUTCHER_YAML_IMPORTER_HELPERS_H
