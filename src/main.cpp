039b630013cca2996058768183b598162283fb5f
Federico Facchinetti <federicoluca.facchinetti@mail.polimi.it>
2023-03-20 17:58:58 +0100

    Fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,9 @@
 #include "General_Manager.h"
-/*
+
 int
 main(int argc, char **argv)
 {
   network_butcher_io::General_Manager::read_command_line(argc, argv);
-
-  return 0;
-}*/
-int
-main(int argc, char **argv)
-{
-  network_butcher_io::General_Manager::boot("test_data/configs/test4_parameters.conf", true);
 
   return 0;
 }
