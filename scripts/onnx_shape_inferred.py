a1ddedebf0b6a06080bd227370482d57be3740b2
Federico Facchinetti <federicoluca.facchinetti@mail.polimi.it>
2023-03-20 11:53:00 +0100

    Added extra example

--- scripts/onnx_shape_inferred.py
+++ scripts/onnx_shape_inferred.py
@@ -1,10 +1,11 @@
 import onnx
+import argparse
 
-base_name = 'mobilenet_v2'
-model_path_no_ext = '../models/' + base_name
-ext_name = '.onnx'
-model_path = model_path_no_ext + ext_name
-inferred_model_path = model_path_no_ext + '-inferred' + ext_name
+parser = argparse.ArgumentParser(prog="Onnx Shape Inferrer")
+parser.add_argument('-i', '--input', type=str, help='Input path of the .onnx model')
+parser.add_argument('-o', '--output', type=str, help='Output path of the .onnx model')
+
+args = parser.parse_args()
 
 # output the inferred model to the specified model path
-onnx.shape_inference.infer_shapes_path(model_path, inferred_model_path)
+onnx.shape_inference.infer_shapes_path(args.input, args.output)
