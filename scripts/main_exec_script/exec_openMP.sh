dd5dba89514cd27bdce9957349ff180b24770cd8
Federico Facchinetti <federicoluca.facchinetti@mail.polimi.it>
2023-06-10 23:30:07 +0200

    Documentation fix

---
+++ scripts/main_exec_script/exec_openMP.sh
@@ -0,0 +1,15 @@
+for i in 2 4 8 16
+do
+   mkdir -p ../results/FINAL/Revision/OpenMP/$i
+   export OMP_NUM_THREADS=$i
+   echo $OMP_NUM_THREADS
+
+
+   ./main_kfinder
+   ./main_synthetic_graph
+   ./main_Constrained_Builder
+   ./main_IO_Manager
+   ./main_path_reconstruction
+   mv report_* ../results/FINAL/Revision/OpenMP/$i
+   
+done
