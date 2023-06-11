845710d8269edacf4ccf87c1011ff20d9a7d09b7
Federico Facchinetti <federicoluca.facchinetti@mail.polimi.it>
2023-06-11 14:21:26 +0200

    Added profiling results

--- scripts/produce_time_network_butcher.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python3 run.py -c example_configurations/butcher_test.ini -o output_butcher
-python3 predict.py -c example_configurations/butcher_predict.ini -r output_butcher/LRRidge.pickle -o output_butcher_predict
